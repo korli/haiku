4341cd1d0532674f97c8e4d64759e27e7346bc6b
Jérôme Duval <jerome.duval@gmail.com>
2016-08-18 09:51:21 +0200

    deploy_to_ghpages.sh verbose

--- build/scripts/deploy_to_ghpages.sh
+++ build/scripts/deploy_to_ghpages.sh
@@ -1,9 +1,9 @@
-#!/usr/bin/env bash
+#!/bin/sh -vx
 
 if [ -n "$GITHUB_TOKEN" ]; then
   cd generated/objects/haiku/$targetArch/packaging/repositories/Haiku && \
   git init && \
   git add . && \
   git commit -c user.name="Travis CI" -c user.email="github@travis-ci.org" -m "Deploy to GitHub Pages" && \
-  git push -q --force "https://$GITHUB_TOKEN@github.com/korli/haiku.git" master:gh-pages
+  git push --force "https://$GITHUB_TOKEN@github.com/korli/haiku.git" master:gh-pages
 fi
