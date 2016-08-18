ec42567fe89453b29a3dde76f78dc3fe2b052333
Jérôme Duval <jerome.duval@gmail.com>
2016-08-18 12:15:49 +0200

    Update deploy_to_ghpages.sh

--- build/scripts/deploy_to_ghpages.sh
+++ build/scripts/deploy_to_ghpages.sh
@@ -5,8 +5,8 @@ targetArch=x86_64
 if [ -n "$GITHUB_TOKEN" ]; then
   cd generated/objects/haiku/$targetArch/packaging/repositories/Haiku && \
   git init && \
-  git config --global user.name "Travis CI" \
-  git config --global user.email "github@travis-ci.org" \
+  git config --global user.name "Travis CI" && \
+  git config --global user.email "github@travis-ci.org" && \
   git add . && \
   git commit -m "Deploy to GitHub Pages" && \
   git push --force "https://$GITHUB_TOKEN@github.com/korli/haiku.git" master:gh-pages
